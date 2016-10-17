139b96f8a8bc99e78a39a13474d69dc08ef0ff0a
Jeff Wolfram <jeffwolfram@gmail.com>
2016-10-17 16:57:04 -0700

    I forgot to include these changes.

---
+++ scripts/utilities.js
@@ -0,0 +1,9 @@
+function forEach(){
+    for(i = 0; i < points.length; i++){
+                points[i].style.opacity = 1;
+                points[i].style.transform = "scaleX(1) translateY(0)";
+                points[i].style.msTransform = "scaleX(1) translateY(0)";
+                points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+        
+}
