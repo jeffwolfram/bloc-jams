f1cb2088a3db722b53f928b32888d64df50dc145
Jeff Wolfram <jeffwolfram@gmail.com>
2016-10-17 16:52:54 -0700

    I am not done with this yet. I am a little confused with the assignment. I am going to ask jose about it tonight and finish this tomorrow.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points){   
-    var revealPoint = function(index) {
+var animatePoints = function (points) {   
+    var revealPoint = function (index) {
                 points[index].style.opacity = 1;
                 points[index].style.transform = "scaleX(1) translateY(0)";
                 points[index].style.msTransform = "scaleX(1) translateY(0)";
