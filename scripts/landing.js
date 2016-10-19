dbb6c1f51d5db607db7116f6f1b3ddceb33c9acd
Jeff Wolfram <jeffwolfram@gmail.com>
2016-10-19 09:10:08 -0700

    still not working. I think the forEach function is not being called.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,20 @@
+function forEach(collection, fn){
+    for(var i = 0; i < collection.length; i++){
+        fn(collection[i]);
+    }
+}
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points){   
-    var revealPoint = function(index) {
-                points[index].style.opacity = 1;
-                points[index].style.transform = "scaleX(1) translateY(0)";
-                points[index].style.msTransform = "scaleX(1) translateY(0)";
-                points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function(points){
+    var revealPoint = function(point){
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
         
     }
-     for(var i = 0; i < points.length; i++){
-         revealPoint(i);
-     }
 };
+
 
 window.onload = function() {
     if (window.innerHeight > 950){
@@ -24,4 +27,7 @@ window.onload = function() {
             animatePoints(pointsArray);
         }
     });
-}
+};
+
+
+
